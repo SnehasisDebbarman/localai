6c81cbd48383eca70627b399b91018bcd8c651d7
mktgsfdclightning <snehasisdebbarman2016@gmail.com>
2025-01-12 20:34:20 +0530

    cors origin fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Proxy all requests starting with /api
+        destination: "http://localhost:11434/api/:path*", // Forward to local server
+      },
+    ];
+  },
 };
 
 export default nextConfig;
